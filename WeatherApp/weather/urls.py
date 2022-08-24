5a6542fa069a52a1f3650d61afe18d260d0524c0
boa72 <olya.borisevich29@gmail.com>
2022-08-24 16:56:47 +0300

    detail info for selected city

--- WeatherApp/weather/urls.py
+++ WeatherApp/weather/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('news', views.news)
+    path('', views.index,name='home'),
+    path('<int:pk>', views.city_detail,name='city_detail')
 ]
--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -3,9 +3,10 @@ import requests
 from .models import City
 from .forms import CityForm
 
+appid="c7ce0415da92323388c1b3bf8120a45c"
+url="https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&lang=ru&appid="+appid
+
 def index(request):
-    appid="c7ce0415da92323388c1b3bf8120a45c"
-    url="https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid="+appid
     if request.method=='POST':
         form=CityForm(request.POST)
         form.save()
@@ -17,16 +18,26 @@ def index(request):
         res=requests.get(url.format(city.name)).json()
         city_info={
             'city':city.name,
+            'pk':city.pk,
             'temp':res['main']['temp'],
-            'feels_like':res['main']['feels_like'],
-            'humidity': res['main']['humidity'],
-            'main':res['weather'][0]['main'],
-            'wind':res['wind']['speed'],
             'icon':res['weather'][0]['icon']
         }
         all_cities.append(city_info)
     context={'all_info':all_cities,'form':form}
+
     return render(request,'weather/index.html',context)
 
-def news(request):
-    return render(request,'weather/news.html')
+def city_detail(request,pk):
+    city=City.objects.get(pk=pk)
+    res = requests.get(url.format(city.name)).json()
+    city_info={
+        'city':city.name,
+        'temp':res['main']['temp'],
+        'feels_like':res['main']['feels_like'],
+        'humidity': res['main']['humidity'],
+        'main':res['weather'][0]['main'],
+        'wind':res['wind']['speed'],
+        'icon':res['weather'][0]['icon']
+        }
+    context={'info':city_info}
+    return render(request,'weather/city_detail.html',context)
